65b2b528b163716beb13e5ee1041535afa14069c
YahyaOsama2612 <oyahya868@gmail.com>
2026-09-08 12:29:09 +0300

    edit size photos

--- components/UI/RestaurantItms.tsx
+++ components/UI/RestaurantItms.tsx
@@ -1548,7 +1548,7 @@ export default function RestaurantItms({
                   alt={selectedItem.name}
                   loading="eager"
                   decoding="async"
-                  className="object-contain w-full h-full transform transition-transform duration-[1000ms] ease-out hover:scale-105 will-change-transform"
+                  className="object-cover w-full h-full transform transition-transform duration-[1000ms] ease-out hover:scale-105 will-change-transform"
                   style={{
                     imageRendering: "-webkit-optimize-contrast",
                     transform: "translate3d(0,0,0)",
